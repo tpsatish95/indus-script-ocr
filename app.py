703d46d72825140ce360307bed1eb20314694a05
tpsatish95 <tpsatish95@gmail.com>
2017-03-24 10:05:03 +0530

    Add region_search to stages and refactor stages into sub stages

--- app.py
+++ app.py
@@ -1,9 +1,46 @@
 import sys
 
-from stages import extract_seal
+import skimage.io
+
+from helpers import logger
+from stages import region_proposal
+
+LOGGER = logger.create_logger(__name__)
+
+
+def get_new_image_dimensions(image):
+
+    LOGGER.info("Calculating the new image dimensions ...")
+
+    img = skimage.io.imread(image.name)
+    width = len(img[0])
+    height = len(img)
+
+    if width * height < 256 * 256 * (0.95) and abs(width - height) <= 3:
+        new_size = 512
+    elif width * height < 220 * 220 * (1.11):
+        new_size = 256
+    elif width * height < 256 * 256:
+        new_size = 256
+    elif width * height > 512 * 512 * (0.99) and width < 800 and height < 800:
+        new_size = 512
+    elif width * height < 512 * 512 * (0.95) and width * height > 256 * 256 * (1.15):
+        new_size = 512
+
+    new_height = int(new_size * height / width)
+    new_width = new_size
+
+
+    return new_width, new_height
+
+
+def process(image_path):
+    seal = region_proposal.extract_seal.crop_white(image_path)
+    new_width, new_height = get_new_image_dimensions(seal)
+
+    candidate_regions = region_proposal.region_search.get_candidate_regions(seal, new_width, new_height)
 
 
 if __name__ == "__main__":
     input_artifact_image_path = sys.argv[1]
-
-    seal_image_path = extract_seal.crop_white(input_artifact_image_path)
+    process(input_artifact_image_path)
---
+++ stages/region_proposal/region_search.py
@@ -0,0 +1,48 @@
+import skimage.io
+import skimage.transform
+
+from helpers import logger
+from lib import selectivesearch
+
+LOGGER = logger.create_logger(__name__)
+
+
+def get_candidate_regions(image, width, height):
+    LOGGER.info("Extracting the candidate regions ...")
+    candidates = set()
+
+    stage = 0
+    for sc in [350, 450, 500]:
+        for sig in [0.8]:
+            for mins in [30, 60, 120]:
+                img = skimage.io.imread(image.name)[:, :, :3]
+                if not (height == len(img) and width == len(img[0])):
+                    img = skimage.transform.resize(img, (height, width))
+
+                _, regions = selectivesearch.selective_search(
+                    img, scale=sc, sigma=sig, min_size=mins)
+
+                for r in regions:
+                    # excluding same rectangle (with different segments)
+                    if r['rect'] in candidates:
+                        continue
+
+                    # excluding regions smaller than 2000 pixels
+                    if r['size'] < 2000:
+                        continue
+
+                    # distorted rects
+                    _, _, w, h = r['rect']
+                    if w / h > 1.2 or h / w > 1.2:
+                        continue
+
+                    # rects covering entire seal image
+                    if w >= (img.shape[0] - 1) * (0.7) and h >= (img.shape[1] - 1) * (0.7):
+                        continue
+
+                    candidates.add(r['rect'])
+
+        LOGGER.info("Stage " + str(stage) + " Complete.")
+        stage += 1
+
+    return candidates
