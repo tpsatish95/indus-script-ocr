d0214dd294a7ef7badb011e7e197a4c6f205a667
tpsatish95 <tpsatish95@gmail.com>
2017-03-27 08:12:00 +0000

    Add get best_text_regions based on different dimensions in main

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import shutil
 import sys
 
 import skimage.io
@@ -33,10 +34,7 @@ def get_new_image_dimensions(image):
     return new_width, new_height
 
 
-def process(image_path):
-    seal = region_proposal.extract_seal.crop_white(image_path)
-    new_width, new_height = get_new_image_dimensions(seal)
-
+def get_text_regions(seal, new_width, new_height):
     candidate_regions = \
         region_proposal.region_search.get_candidate_regions(seal, new_width, new_height)
     grouped_regions = \
@@ -45,6 +43,40 @@ def process(image_path):
         text_region_extraction.region_classification.process_regions(seal, grouped_regions, new_width, new_height)
     formulated_text_regions = \
         text_region_extraction.text_region_formulation.process_regions(text_regions, no_text_regions, both_regions, new_width, new_height)
+
+    return formulated_text_regions
+
+
+def get_best_text_regions(seal, new_width, new_height):
+    orig_image = skimage.io.imread(seal.name)
+    orig_width = len(orig_image[0])
+    orig_height = len(orig_image)
+
+    all_dimensions = set([256, 512, orig_width])
+    tried_dimensions = set()
+
+    while True:
+        tried_dimensions.add(new_width)
+        text_regions = get_text_regions(seal, new_width, new_height)
+
+        # min area check
+        is_less_min_area = False
+        for x, y, w, h in text_regions:
+            if w * h < new_width * new_height * 0.20 and (w < new_width * 0.20 or h < new_height * 0.20):
+                is_less_min_area = True
+
+        if (len(text_regions) == 0 or is_less_min_area) and len(tried_dimensions) < 3:
+            new_width = list(all_dimensions - tried_dimensions)[0]
+            new_height = int(new_width * orig_height / orig_width)
+            LOGGER.info("New size being tried: " + str(new_width))
+
+        else:
+            return text_regions
+
+def process(image_path):
+    seal = region_proposal.extract_seal.crop_white(image_path)
+    new_width, new_height = get_new_image_dimensions(seal)
+    best_text_regions = get_best_text_regions(seal, new_width, new_height)
 
 
 if __name__ == "__main__":
